d7e60f8ecc7cd617e40598bd6093b48106660179
Kaua10710 <kaduzerah06@gmail.com>
2024-10-24 19:49:21 -0300

    Update Emprestimo.java

--- Trabalho1/src/main/java/Trabalho1/Emprestimo.java
+++ Trabalho1/src/main/java/Trabalho1/Emprestimo.java
@@ -1,7 +1,22 @@
 package Trabalho1;
 
+import java.util.Scanner;
+
 public class Emprestimo {
-    String dataEmprestimo;
-    
+String livro;
+boolean emprestado;
+
+public Emprestimo(String livro) {
+ this.livro = livro;
+ this.emprestado = false;
+
+}
+ 
+public void emprestar() {
+   if (!emprestado) { 
+      emprestado = true;
+       System.out.println("Livro " + " emprestado.");
+   }
+}
     
 }
