8918a8b4e29b8583053ad55643aff351eaf92635
Kaua10710 <kaduzerah06@gmail.com>
2024-10-25 20:52:20 -0300

    Update AppEmprestimo.java

--- Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
+++ Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Trabalho1;
 
-/**
- *
- * @author luizk
- */
+import java.util.Scanner; // Import de variavéis.
+
 public class AppEmprestimo {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        //Criando instâncias de Emprestimo para diferentes livros.
+        //Criando classes construtoras
+        Emprestimo emprestimo1 = new Emprestimo("1984");
+        Emprestimo emprestimo2 = new Emprestimo("O pequeno Príncipe");
+        
+        int opcao; //Variável de verificação 
+        
+        do { 
+            System.out.println("\n--- Menu de Empréstimo de Livros ---");
+            System.out.println("1. Emprestar '1984'");
+            System.out.println("2. Devolver '1984'");
+            System.out.println("3. Emprestar 'O Pequeno Príncipe'");
+             System.out.println("4. Devolver 'O Pequeno Príncipe'");
+             System.out.println("0. Sair");
+             System.out.println("Escolha uma opção: ");
+             
+             opcao = scanner.nextInt();
+            
+             // testar supostos casos.
+             switch (opcao) {
+                 case 1: 
+                    
+        }
+            
+          
+    }
 }
