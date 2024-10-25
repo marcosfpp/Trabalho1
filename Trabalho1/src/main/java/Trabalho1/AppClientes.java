6d017ac60e9c48105a981de6aef66f86c505abe3
marcos felipe pessoa <marcos.pessoa@estudante.ifgoiano.edu.br>
2024-10-25 20:43:21 -0300

    estruturação de um APPcliente para adiantamento

---
+++ Trabalho1/src/main/java/Trabalho1/AppClientes.java
@@ -0,0 +1,34 @@
+
+package Trabalho1;
+
+import java.util.Scanner;
+
+public class AppClientes {
+    
+        //criação de função menu
+        public static void menu(){
+                    System.out.println("[=========DEVS LIBRARY========]");
+                    System.out.println("1 - [CADASTRO DE LIVROS]\n");      
+                    System.out.println("2 - [CADASTRO USUÁRIO]\n");
+                    System.out.println("4 - [LISTAGEM DE USUÁRIOS]\n");
+                    System.out.println("4 - [LISTAGEM DE LIVROS]\n");
+                    System.out.println("3 - [EMPRÉSTIMO]\n");
+                    System.out.println("4 - [DEVOLUÇÃO]\n");
+                    System.out.println("5 - [SAIR]\n");
+                    System.out.println("[=============================]");
+
+        }
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            while (opc != 5) {                
+                
+            }
+            
+
+            //Protótipo da estrutura de seleção por parte da escolha numérica do usuário
+
+        }
+
+}
