1776a5008da26edbb239aada2ce1af5d4fdaf739
Kaua10710 <kaduzerah06@gmail.com>
2024-11-03 16:08:41 -0300

    Update AppEmprestimo.java

--- Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
+++ Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
@@ -1,13 +1,12 @@
 
 package Trabalho1;
 
-import java.util.Scanner; // Import de variavéis.
 
 public class AppEmprestimo {
     public static void main(String[] args) {
         //Criando instâncias de usuário e livro para o teste
-        BibliUsuario usuario = new BibliUsuario("Carlos Silva", 101); //Nome e ID do usuário
-        BibliLivro livro = new BibliLivro("O Senhor dos Anéis", 1001); // Título e código do livro 
+        Clientes usuario = new Clientes("Carlos Silva", 101); //Nome e ID do usuário
+        Livros livro = new Livros("O Senhor dos Anéis", 1001); // Título e código do livro 
         
         //Exibindo o estado inicial dos objetos
         System.out.println("Usuário criado: " + usuario.getNome() + ", ID: " + usuario.getId());
