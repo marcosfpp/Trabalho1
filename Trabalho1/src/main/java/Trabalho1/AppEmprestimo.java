1ad747b649f9e128a2a2e8a7f285be266bf35989
Kaua10710 <kaduzerah06@gmail.com>
2024-11-03 15:49:54 -0300

    Update AppEmprestimo.java

--- Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
+++ Trabalho1/src/main/java/Trabalho1/AppEmprestimo.java
@@ -10,4 +10,25 @@ public class AppEmprestimo {
         BibliLivro livro = new BibliLivro("O Senhor dos Anéis", 1001); // Título e código do livro 
         
         //Exibindo o estado inicial dos objetos
+        System.out.println("Usuário criado: " + usuario.getNome() + ", ID: " + usuario.getId());
+        System.out.println("Livro criado: " + livro.getTitulo() + ", Código: " + livro.getCodigo());
+        
+        //criando um empréstimo ativo 
+        Emprestimo emprestimo = new Emprestimo(livro, "01/11/2024", usuario);
+        System.out.println("\nEmpréstimo criado:\n" + emprestimo);
+        
+        //verificando a disponibilidade do livro antes do empréstimo
+        emprestimo.verificarDisponibilidade();
+        
+        //Realizando empréstimo
+        emprestimo.realizarEmprestimo();
+        System.out.println("\nEmpréstimo realizado. Status do empréstimo: " + emprestimo.isEmprestimoAtivo());
+        
+        //Devolvendo o livro
+        emprestimo.devolverLivro();
+        System.out.println("\nLivro devolvido. Status do empréstimo: " + emprestimo.isEmprestimoAtivo());
+        
+        //Exibindo o estado final dos objetos 
+        System.out.println("\nEstado final do empréstimo:\n" + emprestimo);
+        
 }
