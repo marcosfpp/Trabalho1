22f23f91b9a40b6888bf9c7eb5f03fbbf152d46e
markinvns <marcus.nunes@estudante.ifgoiano.edu.br>
2024-11-06 19:57:24 -0300

    Mudança na estrutura do App
    Adição de uma função menu, permissão do usuário para utilizar os cases caso tenha erro, introdução a estrutura do try catch para junção dos demais códigos para a finalização do trabalho

--- Trabalho1/src/main/java/Trabalho1/App.java
+++ Trabalho1/src/main/java/Trabalho1/App.java
@@ -1,53 +1,125 @@
-
 package Trabalho1;
 
-import static Trabalho1.AppClientes.menu;
 import java.util.Scanner;
 
-
-public class App {
-    public static void menu() {
-        System.out.println("[=========DEVS LIBRARY========]");
-        System.out.println("1 - [CADASTRO DE LIVROS]\n");
-        System.out.println("2 - [CADASTRO USUÁRIO]\n");
-        System.out.println("4 - [LISTAGEM DE USUÁRIOS]\n");
-        System.out.println("4 - [LISTAGEM DE LIVROS]\n");
-        System.out.println("3 - [EMPRÉSTIMO]\n");
-        System.out.println("4 - [DEVOLUÇÃO]\n");
-        System.out.println("5 - [SAIR]\n");
-        System.out.println("[=============================]");
-
+public class App{
+    // Criado a função menuinicial com objetivo de mostrar o menu da biblioteca ao usuário.
+    public static void menuinicial() {
+        System.out.println("MENU");
+        System.out.println("1- CADASTRO LIVRO");
+        System.out.println("2- CADASTRO USUÁRIO");
+        System.out.println("3- REALIZAR EMPRÉSTIMO");
+        System.out.println("4- REALIZAR DEVOLUÇÃO");
+        System.out.println("5- LISTAGEM DE LIVROS");
+        System.out.println("6- LISTAGEM DE USUÁRIOS");
+        System.out.println("7- EMPRÉSTIMOS ATIVOS");
+        System.out.println("8- SAIR");
     }
-     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        menu();
-        System.out.println("Escolha a opção desejada: ");
-        int opc = scan.nextInt();
 
-        while (opc != 5) {
-            switch (opc) {
-                case 1:
-                    System.out.println("[CADASTRO DE LIVROS]\n");      
-                    System.out.println("Caso você queira cancelar a operação digite 5 ou 6 para cadastrar novamente");
-                    break;
-                case 2:
-                    System.out.println("[CADASTRO USUÁRIO]\n");
-                    break;
-                case 3:
-                    System.out.println("[EMPRÉSTIMO]");
-                    break;
-                case 4:
-                    System.out.println("Deseja fazer a devolução de um livro: (S ou N)");
-                    String opc_sec = scan.nextLine();
-                    if (opc_sec == "S") {
-                        System.out.println("Digite o código do livro que deseja devolver");
-                    }
-                    
-                    
-                   
-                    break;    
-                case 5:
-                    break;
+    // Inicialização do código principal.
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bem vindo a biblioteca :)");
+
+        // Opção que permiti o usuário conferir se ele quer utilizar o programa ou não.
+        System.out.println("Você deseja utilizar nossa biblioteca, digite S ou N:");
+        String opc_sec = scan.nextLine();
+
+        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+            menuinicial();
+            System.out.println("Digite a opção que deseja prosseguir:");
+            int opc_pri = scan.nextInt();
+
+            // Inicialização da estrutura de escolha do usuário.
+            while (opc_pri != 8) {
+                switch (opc_pri) {
+                    case 1:
+                        System.out.println("Deseja cadastrar um usuário, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            } catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("Deseja cadastrar um livro, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        System.out.println("Deseja fazer o empréstimo de algum livro, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("Deseja fazer a devolução de algum livro, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        System.out.println("Deseja listar todos os livros, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 6:
+                        System.out.println("Deseja listar todos os usuários, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    case 7:
+                        System.out.println("Deseja consultar os empréstimos ativos, digite S ou N");
+                        opc_sec = scan.nextLine();
+                        if ("S".equals(opc_sec) || "s".equals(opc_sec) || "Sim".equals(opc_sec) || "sim".equals(opc_sec)) {
+                            try {
+                            }catch (Exception e) {
+                                System.out.println("Mensagem de erro");
+                            }
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Selecione a opção correta!");
+                        break;
+                }
+
+                // Caso o usuário escreva uma opção que não é desejada.
+                menuinicial();
+                System.out.println("Digite uma opção válida:");
+                opc_pri = scan.nextInt();
+            }
         }
     }
 }
