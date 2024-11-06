5bfe54be9f8e7717b417e631ee837ca4320d8cce
Dheniel Rodrigues Luis <dheniel.luis@estudante.ifgoiano.edu.br>
2024-11-06 20:36:19 -0300

    Criação da lógica de perguntar o usuário caso queira cadastrar mais livros

--- Trabalho1/src/main/java/Trabalho1/AppLivros.java
+++ Trabalho1/src/main/java/Trabalho1/AppLivros.java
@@ -7,6 +7,7 @@ public class AppLivros {
         Livros[] livros = new Livros[2];
         Scanner scan = new Scanner(System.in);
         int opcaoMenu = 0 ;
+        int j = -1;
         
         while (opcaoMenu != 3){
                 System.out.println("Biblioteca dos DEV's");
@@ -20,43 +21,60 @@ public class AppLivros {
                         for (int i = 0; i < 50; i++){
                             System.out.println("\n");
                         }
-         
-                        for(int i = 0; i < livros.length; i++){                            
-                            System.out.println("Digite o título do livro: ");
-                            String nome = scan.nextLine();
-                            System.out.println("\n");
+                        if (livros.length <= 2){
+                            String c = new String(); 
+                            c = "s";
+                            while(c == "s" || c == "S"){
+                                j++;
+                                System.out.println("Digite o título do livro: ");
+                                String nome = scan.nextLine();
+                                System.out.println("\n");
 
-                            System.out.println("Digite o(s) autor(es) do livro: ");
-                            String autor = scan.nextLine();
-                            System.out.println("\n");
+                                System.out.println("Digite o(s) autor(es) do livro: ");
+                                String autor = scan.nextLine();
+                                System.out.println("\n");
 
-                            System.out.println("Digite o ano de publicação: ");
-                            int anoPubli = scan.nextInt();
-                            scan.nextLine();
-                            System.out.println("\n");
+                                System.out.println("Digite o ano de publicação: ");
+                                int anoPubli = scan.nextInt();
+                                scan.nextLine();
+                                System.out.println("\n");
 
-                            System.out.println("Digite a quantidade de exemplares disponíveis: ");
-                            int quantExemplares = scan.nextInt();
-                            scan.nextLine();
-                            System.out.println("\n");
-                    
-                            Livros informacoesLivro = new Livros(nome, autor, anoPubli, quantExemplares);
-                            livros[i] = informacoesLivro;
-                            
-                            System.out.println("\n\n");
+                                System.out.println("Digite a quantidade de exemplares disponíveis: ");
+                                int quantExemplares = scan.nextInt();
+                                scan.nextLine();
+                                System.out.println("\n");
+
+                                Livros informacoesLivro = new Livros(nome, autor, anoPubli, quantExemplares);
+                                livros[j] = informacoesLivro;
+                                
+                                System.out.println(livros.length);
+                                
+                                System.out.println("Deseja continuar? S/N");
+                                c = scan.nextLine();
+                                
+                                System.out.println(livros.length);
+
+                                System.out.println("\n\n");
+                            }
+                            for (int i = 0; i < 50; i++){
+                                System.out.println("\n");
+                            }
                         }
-                        for (int i = 0; i < 50; i++){
-                            System.out.println("\n");
+                        else{
+                            System.out.println("Não e possível cadastrar mais livros!");
                         }
                         break;
                     
                     case 2:
-                        for (int i = 0; i < livros.length; i++){
-                            System.out.println("\n"+ livros[i].getMostrarDadosStrings() +"\n" + livros[i].getMostrarPublicacao() +"\n" + livros[i].getMostrarExemplares());
-                        }     
+                        for(Livros i : livros){
+                            System.out.println(i);
+                        }  
+                        break;
+                        
                     case 3:
                         System.out.println("Obrigado por usar nosso sistema!");
                        break;
+                       
                     default: 
                         System.out.println("Digite uma opção válida! ");
                         break; 
